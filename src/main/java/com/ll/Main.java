3303daca25d8c25c3e04296bcb940b972a2370c0
kk0506014 <kk0506014@gmail.com>
2024-12-05 14:23:31 +0900

    work

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,10 +1,13 @@
 package com.ll;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         lab1();
+        lab2();
     }
 
     private static void lab1() {
@@ -13,6 +16,16 @@ public class Main {
         System.out.println("명령)");
         String cmd = sc.nextLine().trim();
         
+        System.out.println("입력한 명령: " + cmd);
+    }
+
+    private static void lab2() {
+        InputStream in = new ByteArrayInputStream("메롱".getBytes());
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("명령)");
+        String cmd = sc.nextLine().trim();
+
         System.out.println("입력한 명령: " + cmd);
     }
 }
